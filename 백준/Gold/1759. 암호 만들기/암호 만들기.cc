6d6d9d29619a00a84e28f46c1a06614c4be0a208
Jungi-Kim <54887575+ijun17@users.noreply.github.com>
2023-03-13 22:36:00 +0900

    [Gold V] Title: 암호 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.cc
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.cc
@@ -5,13 +5,12 @@ using namespace std;
 
 bool isVowel(char c){return (c=='a'||c=='e'||c=='i'||c=='o'||c=='u');}
 
-void printPassword(string text, int pwLength, int num=-1, string pw="", int vowelCount=0){
-    if(pw.length()==pwLength-1){
-        for(int i=num+1,l=text.length(); i<l; i++)
-            if(vowelCount+isVowel(text[i])>0&&pwLength-vowelCount-isVowel(text[i])>1)cout<<pw<<text[i]<<'\n';
+void printPassword(string text, int pwLength, string pw="", int lastIndex=-1, int vowelCount=0){
+    if(pw.length()==pwLength){
+        if(vowelCount>0 && pwLength-vowelCount>1)cout<<pw<<'\n';
     }else{
-        for(int i=num+1,l=text.length()+(pwLength-pw.length()); i<l; i++)
-            printPassword(text, pwLength, i, pw+text[i], vowelCount+isVowel(text[i]));
+        for(int i=lastIndex+1,l=text.length()-(pwLength-pw.length())+1; i<l; i++)
+            printPassword(text, pwLength, pw+text[i], i, vowelCount+isVowel(text[i]));
     }
 }
 
