04f8aa38152b2d56f8a21a801fda4c1b3cc728d3
Jungi-Kim <54887575+ijun17@users.noreply.github.com>
2023-03-14 22:59:29 +0900

    [Silver III] Title: 소수 구하기, Time: 8 ms, Memory: 2868 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -1,25 +1,21 @@
 #include <iostream>
-#define loop(i,n) for(int i=0; i<n; i++)
+#include <cmath>
 using namespace std;
 
 int main(){
-    int M,N;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int M,N,l;
     cin>>M>>N;
-    int temp;
     bool decimals[N+1]={0,};
     decimals[0]=1;
     decimals[1]=1;
-    for(int i=2; i<N; i++ ){
-        if(decimals[i]==0)for(int j=2; j<N; j++){
-            temp=i*j;
-            if(temp<=N){
-                decimals[temp]=1;
-            }else break;
-        }   
+    for(int i=2, l1=ceil(sqrt(N))+1; i<l1; i++){
+        if(!decimals[i])for(int j=i*i, l2=N+1; j<l2; j+=i)decimals[j]=1;
     }
 
-    for(int i=M; i<=N; i++){
-        if(decimals[i]==0)cout<<i<<'\n';
+    for(int i=M, l=N+1; i<l; i++){
+        if(!decimals[i])cout<<i<<'\n';
     }
     return 0;
 }
