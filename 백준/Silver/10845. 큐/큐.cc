29ed946009ffbf92968536be570041437f169506
Jungi-Kim <54887575+ijun17@users.noreply.github.com>
2023-05-08 17:35:30 +0900

    [Silver IV] Title: 큐, Time: 0 ms, Memory: 2064 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int dat[10010],head=0,tail=0;
+
+int empty(){return head==tail;}
+void push(int data){dat[tail++]=data;}
+int pop(){if(empty())return -1;else return dat[head++];}
+int size(){return tail-head;}
+int front(){if(empty())return -1;else return dat[head];}
+int back(){if(empty())return -1;else return dat[tail-1];}
+
+
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N, temp;
+    string s;
+    cin>>N;
+    while(N--){
+        cin>>s;
+        switch (s[1])
+        {
+        case 'u': //push
+            cin>>temp;
+            push(temp);
+            break;
+        case 'o': //pop
+            cout<<pop()<<'\n';
+            break;
+        case 'i': //size
+            cout<<size()<<'\n';
+            break;
+        case 'a': //back
+            cout<<back()<<'\n';
+            break;
+        case 'r': //front
+            cout<<front()<<'\n';
+            break;
+        case 'm'://empty
+            cout<<empty()<<'\n';
+            break;
+        default:
+            break;
+        }
+    }
+    return 0;
+}
